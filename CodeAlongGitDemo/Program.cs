f70ad149ccfb3e953caf9af4db9d5f121e65faf0
ÄGARE <GARE@DESKTOP-K7V7LGF>
2024-10-15 09:32:41 +0200

    Add project files.

---
+++ CodeAlongGitDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeAlongGitDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
